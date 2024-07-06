1c4d9de76efae7562e5a38165680934892a7198c
ranshibo <195523241@qq.com>
2024-07-06 16:47:09 +0800

    feat: redis muti cmd

--- dao/redis/cmd.go
+++ dao/redis/cmd.go
@@ -3,6 +3,9 @@ package redis
 import (
 	"context"
 	"fmt"
+	"time"
+
+	goRedis "github.com/go-redis/redis/v8"
 )
 
 const (
@@ -24,7 +27,46 @@ func MutiGet(ctx context.Context, keys []string) (map[string]string, error) {
 	length := len(keys)
 	m := make(map[string]string)
 	for offset < length {
-		offset = splitKeys(offset, length)
+		netxOffset := splitKeys(offset, length)
+		pipe := GetCommunityClient().Pipeline()
+		for i := offset; i < netxOffset; i++ {
+			pipe.Get(ctx, keys[i])
+		}
+		offset = netxOffset
+
+		cmders, err := pipe.Exec(ctx)
+		if err != nil && err != goRedis.Nil {
+			continue
+		}
+		m := make(map[string]string)
+		for i, cmder := range cmders {
+			result, err := cmder.(*goRedis.StringCmd).Result()
+			if err != nil {
+				continue
+			}
+			m[keys[i]] = result
+		}
 	}
 	return m, nil
+}
+
+func MutiSet(ctx context.Context, keys, values []string, expireTime time.Duration) bool {
+	if len(keys) != len(values) {
+		return false
+	}
+	offset := 0
+	length := len(keys)
+	for offset < length {
+		netxOffset := splitKeys(offset, length)
+		pipe := GetCommunityClient().Pipeline()
+		for i := offset; i < netxOffset; i++ {
+			pipe.Set(ctx, keys[i], values[i], expireTime)
+		}
+		offset = netxOffset
+		_, err := pipe.Exec(ctx)
+		if err != nil {
+			return false
+		}
+	}
+	return true
 }
